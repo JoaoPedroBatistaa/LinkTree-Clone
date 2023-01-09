1bd7ddfc1e9d3fdb09043ede41866224325ef635
João Pedro Batista <joaopbatistastos@gmail.com>
2023-01-09 19:03:39 -0300

    second commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,3 +16,23 @@ async function copyText(e) {
 
 shareButtons.forEach(shareButton => 
   shareButton.addEventListener('click', copyText))
+
+
+const upShareButton = document.querySelectorAll('.share-button')
+console.log(upShareButton)
+
+async function copyText(e) {
+// prevent button going to the site
+  e.preventDefault()
+  const link = this.getAttribute('link')
+
+  try {
+    await navigator.clipboard.writeText(link)
+    alert("Link copiado para área de transferência")
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+upShareButton.forEach(upShareButton => 
+  upShareButton.addEventListener('click', copyText))
